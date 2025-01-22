2d232601a1f0fd1aeaa01976c24afd91e258f641
YVictorin <vicyonjou@gmail.com>
2025-01-22 15:21:44 -0500

    build: add additional configs for production

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // defining backend server port so no need to hard code routes in client
+  // defining backend proxy so no need to hard code routes in server and get to around CORS
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       }
