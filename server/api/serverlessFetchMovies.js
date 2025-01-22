e175bc3a66a9e7513773f9403d7c8043cb9a40e3
YVictorin <vicyonjou@gmail.com>
2025-01-22 14:50:39 -0500

    feat(server): refactor server for local and Vercel compatibility

---
+++ server/api/serverlessFetchMovies.js
@@ -0,0 +1,14 @@
+const convertToId = require('../utils/convertToId');
+const getSimilarGenresMovies = require('../utils/similarGenre');
+
+export default async function handler(req, res) {
+    const userOption = req.query?.userGenre;
+
+    try {
+        const idArr = await convertToId(userOption);
+        const results = await getSimilarGenresMovies(idArr);
+        res.status(200).json(results);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to fetch similar movies: " + error.message });
+    }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-  // defining backend server port so no need to hard code routes in client 
+
   export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     proxy: {
+        // defining backend server port so no need to hard code routes in client or server.js file
       '/api': {
         target: mode === 'development' 
           ? 'http://localhost:3000'  // Local backend during development
